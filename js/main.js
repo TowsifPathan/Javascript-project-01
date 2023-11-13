f79886decccfea868cb556bac29e04a78d74aab7
Towsif18 <towsif678@gmail.com>
2023-11-14 03:20:41 +0600

    res till 1000

--- js/main.js
+++ js/main.js
@@ -1,58 +1,67 @@
-let pOne = document.querySelector(".pOne")
-let pTwo = document.querySelector(".pTwo")
-let main = document.querySelector(".main")
-let input = document.querySelector('.input')
-let btn = document.querySelector('.btn')
-let cautionOne = document.querySelector(".cOne")
-let cautionTwo = document.querySelector(".cTwo")
-let cautionOnee = document.querySelector(".cOnee")
-let cautionTwoo = document.querySelector(".cTwo")
-let btnTwo = document.querySelector('btnTwo')
-let afBG = document.querySelector(".afBG")
-let pol = document.querySelector('.pol')
-let h5 = document.querySelector('.h5')
-let inputTwo = document.querySelector('.inputTwo')
-let fde = document.querySelector('.fde')
-let jafde =document.querySelector('.jafde')
-btn.addEventListener('click', () => {
-    if (input.value == '') {
-        cautionTwo.style.display = "block";
-    }else if (input.value > 10) {
-        cautionOne.style.display = "block";
+// player one 
+let plOneInterFace = document.querySelector('.pl_one_interface')
+let p1Title = document.querySelector('.p1_title')
+let playerOne = document.querySelector('.playerOne')
+let plOneInput = document.querySelector('.pl_one_input')
+let secondCaution = document.querySelector('.second_caution')
+let firstCaution = document.querySelector('.first_caution')
+let plOneBtn = document.querySelector('.pl_one_btn')
+let p1winner = document.querySelector('.p1winner')
 
+// player one game functions
+
+plOneBtn.addEventListener('click' , ()=>{
+    if (plOneInput.value == ''){
+        firstCaution.style.display = "block"
     }
-    else if (input.value < 10) {
-        pOne.style.display = "none"
-        pTwo.style.display = "block"
-        btn.style.display = "none"
-        
+    else if (plOneInput.value > 10){
+        secondCaution.style.display = "block"
+    }
+    else if (plOneInput.value < 10){
+        plOneInterFace.style.opacity = ".5"
+        plTwoInterFace.style.transform = "translateY(-100%)"
     }
 })
 
-function Button() {
-    if (inputTwo.value == '') {
-        cautionTwoo.style.display = "block";
-    }
-    else if (inputTwo.value > 10) {
-        cautionOnee.style.display = "block";
+function erase() {
+    firstCaution.style.display = "none"
+    secondCaution.style.display = "none"
+}
 
-    }
-    else if (inputTwo.value < input.value) {
-        afBG.style.display = "block"
-        h5.style.display = "none"
-        fde.style.display = "none"
 
+// player two
+let plTwoInterFace = document.querySelector('.pl_two_interface')
+let p2Title = document.querySelector('.p2_title')
+let playerTwo = document.querySelector('.playerTwo')
+let plTwoInput = document.querySelector('.pl_two_input')
+let p2secondCaution = document.querySelector('.p2_second_caution')
+let p2firstCaution = document.querySelector('.p2_first_caution')
+let plTwoBtn = document.querySelector('.pl_two_btn')
+let p2winner = document.querySelector('.p2winner')
+
+
+// player two game function
+
+plTwoBtn.addEventListener('click' , ()=>{
+    if (plTwoInput.value == ''){
+        p2firstCaution.style.display = "block"
     }
-    else if(inputTwo.value == input.value) {
-        afBG.style.display = "block"
-        pol.style.display = "none"
+    else if (plTwoInput.value > 10){
+        p2secondCaution.style.display = "block"
     }
-    else if (inputTwo.value > input.value) {
-        afBG.style.display = "block"
-        h5.style.display = "none"
-        jafde.style.display = "none"
+    else if (plTwoInput.value != plOneInput.value){
+        plOneInterFace.style.opacity = "1"
+        plTwoInterFace.style.transform = "translateY(100%)"
+        p1winner.style.transform = "translateY(30%)"
     }
-   
+    else if (plTwoInput.value == plOneInput.value){
+        p2winner.style.transform = "translateY(30%)"
+    }
+})
+
+function erase2() {
+    p2firstCaution.style.display = "none"
+    p2secondCaution.style.display = "none"
 }
 
 
