f9d6adbc842e4c32111651dccc656c2cd535d02c
Towsif18 <towsif678@gmail.com>
2023-11-18 05:14:12 +0600

    css design

--- js/main.js
+++ js/main.js
@@ -52,10 +52,10 @@ plTwoBtn.addEventListener('click' , ()=>{
     else if (plTwoInput.value != plOneInput.value){
         plOneInterFace.style.opacity = "1"
         plTwoInterFace.style.transform = "translateY(100%)"
-        p1winner.style.transform = "translateY(30%)"
+        p1winner.style.transform = "translateY(32%)"
     }
     else if (plTwoInput.value == plOneInput.value){
-        p2winner.style.transform = "translateY(30%)"
+        p2winner.style.transform = "translateY(32%)"
     }
 })
 
